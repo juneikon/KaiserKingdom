30df185eb68169329b4bc87232eb910b532648c5
juneikon <136241918+juneikon@users.noreply.github.com>
2023-07-08 12:39:50 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,34 @@
+import random
+
+def get_choice():
+   player_choice = input("Enter a choice(rock, paper, scissors:")
+   options = ["rock", "paper" , "scissors"]
+   computer_choice = random.choice(options)
+   choices = {"player" : player_choice, "computer" : computer_choice}
+   return choices
+
+def check_win(player , computer):
+  print(f"you chose {player}, computer chose  {computer}")
+  if player==computer:
+   return "It's a tie!"
+  elif player=="rock": 
+    if computer=="scissors":
+      return "Rock smashes scissors! You win!"
+    else:
+      return "Paper covers rock! You lose!"
+
+  elif player=="paper": 
+    if computer=="scissors":
+      return "Scissors cuts paper! You lose!"
+    else:
+      return "Paper covers rock! You win!"
+      
+  elif player=="scissors" :
+    if computer=="paper":
+      return "Scissors cuts paper! You win!"
+    else:
+      return "Rock smashes scissors! You lose!"
+          
+choices = get_choice()
+result = check_win(choices["player"], choices["computer"])
+print(result)
